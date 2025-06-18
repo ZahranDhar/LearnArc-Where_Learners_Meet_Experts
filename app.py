0a5233e7a36a4fd8a01d0bac905b84cc4707113b
Zahran Dhar <zahrannazirdhar@gmail.com>
2025-06-18 21:45:48 +0530

    Update

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 # Handle Open Website Request
 @app.route('/')
 def form():
-    return render_template("signup.html")
+    return render_template('login.html')
 
 # Handle Signup Request 
 @app.route('/signUp', methods=['POST'])
