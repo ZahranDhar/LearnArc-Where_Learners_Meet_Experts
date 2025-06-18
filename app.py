6d44348f136a69ff22f6cd0e67fa0ce3d4589d94
Zahran Dhar <zahrannazirdhar@gmail.com>
2025-06-19 02:43:29 +0530

    AWS+MongoDb-Added

--- app.py
+++ app.py
@@ -1,31 +1,51 @@
 from flask import Flask, request, render_template
 import os
+from pymongo import MongoClient
+import bcrypt
+import boto3
 
-app = Flask(__name__)
+# Connecting to AWS Server
+s3 = boto3.client('s3', aws_access_key_id='', aws_secret_access_key='', region_name='eu-north-1' )
 
-# Folder to save uploaded images
-UPLOAD_FOLDER = 'uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+# Connecting to MongoDB
+client = MongoClient("mongodb+srv://zahrannazirdhar:@cluster.wojbkyb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster")
+db = client["LearnArc-StorageMongoDB"]
+collection = db["Profiles"]
 
 # Handle Open Website Request
+app = Flask(__name__)
 @app.route('/')
-def form():
-    return render_template('login.html')
+def welcome():
+
+    return render_template('signup.html')
 
 # Handle Signup Request 
+
+# Uploads Profile picture to AWS
+def uploadProfilePictureToAWS(fileObj):
+
+    fileName = f"profile_pictures/{fileObj.filename}"
+    bucketName = 'learnarc-storageaws'
+    s3.upload_fileobj(fileObj, bucketName, fileName, ExtraArgs={'ContentType': fileObj.content_type})
+
+    return f"https://{bucketName}.s3.amazonaws.com/{fileName}"
+
+# Uploads Data to Databases
 @app.route('/signUp', methods=['POST'])
 def signUp():
-    name = request.form['name']
-    username = request.form['username']
-    password = request.form['password']
-    age = request.form['age']
-    
-    profilePicture = request.files['profilepicture']
-    if profilePicture:
-        file_path = os.path.join(UPLOAD_FOLDER, profilePicture.filename)
-        profilePicture.save(file_path)
+
+    # Secure Password
+    securepassword = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+
+    # Upload Profile Picture to AWS
+    file = request.files['profilepicture']
+    if file:
+        url = uploadProfilePictureToAWS(file)
+
+    collection.insert_one({"name": request.form['name'], "username": request.form['username'],"age": request.form['age'],"securepassword": securepassword,"profilepictureurl": url})
     
     return render_template('Home.html')
 
+# Runs app
 if __name__ == '__main__':
     app.run(debug=True)
