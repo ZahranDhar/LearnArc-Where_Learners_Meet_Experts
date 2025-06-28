afb5cec028f1cc88dfc82eff1d70c06c702514cc
Zahran Dhar <zahrannazirdhar@gmail.com>
2025-06-28 11:57:29 +0530

    Profile-Backend-Update

--- routes/Profile.py
+++ routes/Profile.py
@@ -1,33 +1,35 @@
-from flask import request,jsonify, render_template
+from flask import request, jsonify
 
 class ProfileRoutes:
-  def __init__(self,app,db):
-    profiles=db['Profiles']
-    expertises=db['Expertises']
+    def __init__(self, app, db):
+        profiles = db['Profiles']
+        expertises = db['Expertises']
 
-    # Get Profile Route
-    @app.route('/profile',methods=['POST'])
-    def getProfile():
+        @app.route('/profile', methods=['GET'])
+        def getProfile():
+            
+            username = request.args.get('username')
+            user = profiles.find_one({"username": username})
 
-      # Retrieve user data from MongoDB
-      user=profiles.find_one({"username":request.args.get('username')})
-      user['_id']=str(user['_id'])
-      
-      del user['securepassword']
-      del user['chatUsernames']
+            del user['securepassword']
+            del user['_id']
 
-      issideprofile=request.args.get('isSideProfile').lower()=='true'
-      if(issideprofile==False):
-          expertiseIDs=user.get("expertiseIDs",[])
-          expertiseList=list(expertises.find({"_id":{"$in":expertiseIDs}}))
+            issideprofile = request.args.get('isSideProfile', 'false').lower() == 'true'
 
-          for expertise in expertiseList:
-              expertise['_id'] = str(expertise['_id'])
+            if (not issideprofile):
+                
+                expertise_ids = user.get("expertiseIDs", [])
 
-          del user['expertiseIDs']
-          return jsonify({
-          "expertiseList": expertiseList,
-          "user": user
-          })
-      return jsonify(user)
-      
+                expertise_list = list(expertises.find({"_id": {"$in": expertise_ids}}))
+
+                del user['expertiseIDs']
+
+                for exp in expertise_list:
+                    exp['_id']=str(exp['_id'])
+
+                return jsonify({
+                    "user": user,
+                    "expertiseList": expertise_list
+                })
+
+            return jsonify(user)
